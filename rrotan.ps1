b150dfcacd259ec8328e0a6a05d5a8e8c87b8a54
peach-studio <nick.amao@foxmail.com>
2019-01-21 22:28:42 +0800

    fix

--- rrotan.ps1
+++ rrotan.ps1
@@ -10,12 +10,4 @@ xcopy E:\rrotan\public\*.* E:\rrotan.github.io /d/e/y
 
 remove-item E:\rrotan\public\post\* -recurse
 
-remove-item E:\rrotan.coding.net\post\* -recurse
-
 hugo --theme=onetwothree --baseUrl="https://chinae.com.cn"
-
-xcopy E:\rrotan\public\*.* E:\rrotan.coding.net /d/e/y
-
-remove-item E:\rrotan\public\post\* -recurse
-
-hugo --theme=onetwothree --baseUrl="https://rrotan.netlify.com"
