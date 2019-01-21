2b6e3d3d2fb2af4e210ebef1ea9c429e8a944284
peach-studio <nick.amao@foxmail.com>
2019-01-21 21:32:44 +0800

    add 2

--- rrotan.ps1
+++ rrotan.ps1
@@ -1,9 +1,21 @@
-cd D:\Workspace\rrotan
+cd E:\rrotan
 
-remove-item D:\Workspace\rrotan\public\post\* -recurse
+remove-item E:\rrotan\public\post\* -recurse
 
-remove-item D:\Workspace\rrotan.github.io\post\* -recurse
+remove-item E:\rrotan.github.io\post\* -recurse
 
-hugo --theme=onetwothree --baseUrl="https://rrotan.github.io/"
+hugo --theme=onetwothree --baseUrl="https://rrotan.github.io"
 
-xcopy D:\Workspace\rrotan\public\*.* D:\Workspace\rrotan.github.io /d/e/y
+xcopy E:\rrotan\public\*.* E:\rrotan.github.io /d/e/y
+
+remove-item E:\rrotan\public\post\* -recurse
+
+remove-item E:\rrotan.coding.net\post\* -recurse
+
+hugo --theme=onetwothree --baseUrl="https://chinae.com.cn"
+
+xcopy E:\rrotan\public\*.* E:\rrotan.coding.net /d/e/y
+
+remove-item E:\rrotan\public\post\* -recurse
+
+hugo --theme=onetwothree --baseUrl="https://rrotan.netlify.com"
